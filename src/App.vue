a580ce83e32469574203abcdacc639328f12b28f
David Ciria <dciria@ackcent.com>
2022-08-23 10:39:54 +0200

    upload frontend

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <router-link v-if="authenticated" to="/login" v-on:click="logout()" replace>Logout</router-link>
+        </div>
+        <router-view :usernamefk="mockAccount.username" :response="data" :passwordfk="mockAccount.password" @authenticated="setAuthenticated" />
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                authenticated: false,
+                mockAccount: {
+                    username: "nraboy",
+                    password: "password"
+                },
+                data: "null"
+            }
+        },
+        mounted() {
+            axios.get("https://es.wisuki.com/spot/2731/cubelles").then(resp => {
+                console.log(resp['data'])
+                this.data = resp['data']
+            });
+            console.log("mounted");
+            var auth = localStorage.getItem("authenticated");
+            console.log(auth);
+            if(auth == "false") {
+                this.$router.push({ name: "login" });
+            }else{
+                this.authenticated = true;
+                this.$router.push({ name: "secure"});
+            }
+        },
+        methods: {
+            setAuthenticated(status) {
+                console.log("Set auth executed");
+                this.authenticated = status;
+            },
+            logout() {
+                this.authenticated = false;
+                localStorage.setItem("authenticated", false);
+            }
+        }
+    }
+</script>
+
+<style>
+    body {
+        background-color: #F0F0F0;
+    }
+    h1 {
+        padding: 0;
+        margin-top: 0;
+    }
+    #app {
+        width: 1024px;
+        margin: auto;
+    }
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="login">
+        <h1>Login</h1>
+        <div><input type="text" name="username" v-model="input.username" placeholder="Username" /></div>
+        <div><input type="password" name="password" v-model="input.password" placeholder="Password" /></div>
+        <div><button type="button" v-on:click="login()">Login</button></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        props: ["usernamefk", "passwordfk", "response"],
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        emits:['authenticated'],
+        watch: {
+            response() {
+                this.input.username = this.response;
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    console.log(this.usernamefk)
+                    if(this.input.username == this.usernamefk && this.input.password ==this.passwordfk) {
+                        this.$emit("authenticated", true);
+                        localStorage.setItem("authenticated", true);
+                        this.$router.push({ name: "secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+</style>
